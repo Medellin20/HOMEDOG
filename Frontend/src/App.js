4454502e9f3051d88d8ada2ce201bb0364ba744d
Medellin <zola-eric.wendeou@epitech.eu>
2024-03-05 18:49:17 +0100

    update files

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
+
 import DogList from './components/DogList';
 import DogDetails from './components/DogDetails';
 

78bd9b4edb1e6f2190c8c28a4bc5e1af070fa925
Medellin <zola-eric.wendeou@epitech.eu>
2024-03-05 18:27:46 +0100

    update files

--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -41,9 +41,7 @@ const Navbar = () => {
           </span>
         </Link>
         <ul>
-          <li>
-            <Link to="/">Accueil</Link>
-          </li>
+        
           <li>
             <Link to="/dogs">Chiens à vendre</Link>
           </li>
